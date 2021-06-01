12b1cc050fa2008141196dde0e62a7d385344565
kimts2627 <kimtstststs@gmail.com>
2021-06-01 10:16:13 +0900

    [Frontend] <feature>: Add attack function
    Explode class가 추가 되었습니다.
    space key 입력 시 미사일이 지정 좌표로 날아가 폭발 하도록
    이펙트 및 이벤트가 추가 되었습니다.
    추후, key event가 아닌 ai가 플레이 할 수 있도록 refactor 예정 입니다.

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame as pg
 import sys
 from os import path
 from globals import *
+import random
 
 pg.init()
 clock = pg.time.Clock()
@@ -13,40 +14,82 @@ title_rect.y = 510
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 background = pg.image.load('images/background.jpeg')
 pg.display.set_caption('BATTLE-SHIP')
-sprite_group = pg.sprite.Group()
+
+back_group = pg.sprite.Group()
+red_ships = pg.sprite.Group()
+blue_ships = pg.sprite.Group()
 missile_group = pg.sprite.Group()
+effect_group = pg.sprite.Group()
 
 class Missile(pg.sprite.Sprite):
-    def __init__(self, col, row):
+    def __init__(self, col, row, team, target):
         pg.sprite.Sprite.__init__(self)
+        self.team = team
+        self.target = target
         self.image = pg.image.load(path.join('images', 'missile.png')).convert_alpha()
+        if team == 'red': self.image = pg.transform.flip(self.image, True, False)
         self.rect = self.image.get_rect()
         self.rect.x = col
         self.rect.y = row
-        self._layer = 2
-    def moveto_target(self):
-        print('')
+        self.speed = 10
+
+    def update(self):
+        print('update!')
+        x = self.target.x
+        y = self.target.y
+        if self.rect.x != x:
+            print(f'x는 {self.rect.x}')
+            if self.rect.x > x:
+                self.rect.x = self.rect.x - self.speed
+            elif self.rect.x < x:
+                self.rect.x = self.rect.x + self.speed
+        if self.rect.y != y:
+            print(f'y는 {self.rect.y}')
+            if self.rect.y > x:
+                self.rect.y = self.rect.y - self.speed
+            elif self.rect.y < x:
+                self.rect.y = self.rect.y + self.speed
+        if self.rect.y == y and self.rect.x == x:
+            print('hit!!!!!!!!!')
+            explode = Explode(x, y)
+            effect_group.add(explode)
+            missile_group.remove(self)
+
+class Explode(pg.sprite.Sprite):
+    def __init__(self, col, row):
+        pg.sprite.Sprite.__init__(self)
+        self.current_img = 1
+        self.image = pg.image.load(path.join('images', 'effects', f'explode{self.current_img}.png')).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.centerx = col
+        self.rect.centery = row
+    def update(self):
+        if self.current_img < 8:
+            self.current_img += 1
+        elif self.current_img == 8:
+            effect_group.empty()
+        self.image = pg.image.load(path.join('images', 'effects', f'explode{self.current_img}.png')).convert_alpha()
 
 class Ship(pg.sprite.Sprite):
-    def __init__(self, col, row, direction):
+    def __init__(self, col, row, team):
         pg.sprite.Sprite.__init__(self)
+        self.team = team
         self.image = pg.image.load(path.join('images', 'destroyer.png')).convert_alpha()
-        if direction == 'right': self.image = pg.transform.flip(self.image, True, False)
+        if self.team == 'red': self.image = pg.transform.flip(self.image, True, False)
         self.rect = self.image.get_rect()
         self.rect.x = col
         self.rect.y = row
         self._layer = 1
-    def attack(self):
+    def attack(self, target):
         x = self.rect.x
         y = self.rect.y
-        missile = Missile(x, y)
-        sprite_group.add(missile)
+        missile = Missile(x, y, self.team, target)
+        missile_group.add(missile)
 
-
-ship = Ship(150, 150, 'left')
-sprite_group.add(ship)
-ship2 = Ship(900, 250, 'right')
-sprite_group.add(ship2)
+ship = Ship(150, 150, 'blue')
+blue_ships.add(ship)
+ship2 = Ship(900, 250, 'red')
+red_ships.add(ship2)
 
 class Blank(pg.sprite.Sprite):
   def __init__(self, col, row):
@@ -78,16 +121,17 @@ for col in range(0, len(map_data)):
     for row in range(0, len(map_data[col][0])):    
         if map_data[col][0][row] == 'o':
             sea = Sea(col, row)
-            sprite_group.add(sea)
+            back_group.add(sea)
         if map_data[col][0][row] == 'x':
             blank = Blank(col, row)
-            sprite_group.add(blank)
+            back_group.add(blank)
 
 def draw_grid():
     for x in range(0, WIDTH, TILESIZE):
         pg.draw.line(screen, (0, 0, 0, 50), (x, 0), (x, 500))
     for y in range(0, 500, TILESIZE):
         pg.draw.line(screen, (0, 0, 0, 50), (0, y), (WIDTH, y))
+    back_group.draw(screen)
 
 done = False
 while not done:
@@ -97,14 +141,20 @@ while not done:
             done = True
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
-                ship.attack()
+                ships_num = len(red_ships.sprites())
+                ship = blue_ships.sprites()[random.randrange(0, ships_num)]
+                ship.attack(red_ships.sprites()[0].rect)
     screen.fill(BLACK)
     
     screen.blit(background, (0, -176))
     screen.blit(message_title, title_rect)
-    sprite_group.draw(screen)
-    missile_group.draw(screen)
     draw_grid()
+    red_ships.draw(screen)
+    blue_ships.draw(screen)
+    missile_group.draw(screen)
+    missile_group.update()
+    effect_group.draw(screen)
+    effect_group.update()
     pg.display.update()
 pg.quit()
 sys.exit()
